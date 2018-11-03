a791f127ff4760cbbf43c19a9816f47193d6ed15
bbrown005 <bbrown004@student.franciscan.edu>
2018-11-03 00:50:30 -0400

    Added config; added login/reg pages; added footer

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+	// Allow the config
+	define('__CONFIG__', true);
+	// Require the config
+	require_once "inc/config.php"; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -15,39 +22,17 @@
 
 	<body>
 
-
-
 		<div class="uk-section uk-container">
-			<div class=".uk-grid.uk-child-width-1-3@s.uk-child-width-1-1" uk-grid="">
-				<form class="uk-form-stacked">
-
-					<div class="uk-margin">
-						<label class="uk-form-label" for="form-stacked-text">Email</label>
-						<div class="uk-form-controls">
-							<input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-						</div>
-					</div>
-
-					<div class="uk-margin">
-						<label class="uk-form-label" for="form-stacked-text">Password</label>
-						<div class="uk-form-controls">
-							<input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-						</div>
-					</div>
-
-					<div class="uk-margin">
-						<button class="button.uk-button.uk-botton-default" type="submit">Login</button>
-					</div>
-				</form>
-			</div>
+            <?php 
+                echo "Hello World. Today is: ";
+                echo date("Y m d");
+            ?>
+			<p>
+				<a href="/login.php">Login</a>
+				<a href="/register.php">Register</a>
+			</p>
 		</div>
 
-			<!--jQuery is required -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-			<!-- UIkit JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit-icons.min.js"></script>
-
+		<?php require_once "inc/footer.php"; ?>
 	</body>
 </html>
